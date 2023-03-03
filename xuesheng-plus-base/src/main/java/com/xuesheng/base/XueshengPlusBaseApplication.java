2449fb4fd50afb610fbd3a901ea0ed5cbb74fe1b
hand_wenqiang.chen <649418826@qq.com>
2023-03-03 11:41:41 +0800

    Initial commit

---
+++ xuesheng-plus-base/src/main/java/com/xuesheng/base/XueshengPlusBaseApplication.java
@@ -0,0 +1,13 @@
+package com.xuesheng.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XueshengPlusBaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XueshengPlusBaseApplication.class, args);
+    }
+
+}
---
+++ xuesheng-plus-base/src/test/java/com/xuesheng/base/XueshengPlusBaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xuesheng.base;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XueshengPlusBaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
